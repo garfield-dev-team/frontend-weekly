c275d53ae878b2f26e583b1df682ec8bedad00ac
Jiacheng787 <Jiacheng787@users.noreply.github.com>
2022-01-02 12:49:11 +0000

    feat: update pinned section

--- assets/js/3ef0f2a6.266ed181.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfrontend_weekly=self.webpackChunkfrontend_weekly||[]).push([[2436],{1926:function(e){e.exports=JSON.parse('{"permalink":"/frontend-weekly/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
