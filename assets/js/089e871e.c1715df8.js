25bd1ec7b893bebd2b982f2c2c289084b887391c
Jiacheng787 <Jiacheng787@users.noreply.github.com>
2022-01-10 05:58:28 +0000

    Update index.md

---
+++ assets/js/089e871e.c1715df8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend_weekly=self.webpackChunkfrontend_weekly||[]).push([[8833],{7874:function(e){e.exports=JSON.parse('{"allTagsPath":"/frontend-weekly/blog/tags","slug":"/frontend-weekly/blog/tags/rollup","name":"Rollup","count":1,"permalink":"/frontend-weekly/blog/tags/rollup"}')}}]);
---
+++ assets/js/3a0f6c60.f422a83f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend_weekly=self.webpackChunkfrontend_weekly||[]).push([[7848],{1160:function(e){e.exports=JSON.parse('{"allTagsPath":"/frontend-weekly/blog/tags","slug":"/frontend-weekly/blog/tags/babel","name":"Babel","count":1,"permalink":"/frontend-weekly/blog/tags/babel"}')}}]);
