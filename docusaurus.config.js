c63bc029a986be340ca5da36f08b75007a0dc7c3
Jiacheng Dong <2334173653@qq.com>
2022-05-01 17:13:02 +0800

    build: 支持 netlify 部署

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Frontend Weekly',
   tagline: '⭐️ 每周更新优质技术文章，欢迎点赞关注！',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/frontend-weekly/',
+  baseUrl: process.env.IS_NETLIFY === 'true' ? '/': '/frontend-weekly/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
