013ac68477e7e49ce195a2b3035ca65a32d182f1
Jiacheng Dong <2334173653@qq.com>
2022-07-02 12:10:58 +0800

    chore: deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const DeployConfig = require("./deployConfig.js");
 
 const getDeployConfig = () => {
+  console.log('===process.env.VERCEL', process.env.VERCEL);
   if (process.env.VERCEL === 'true') {
     console.log('===app build with: VERCEL===');
     return DeployConfig.VERCEL;
