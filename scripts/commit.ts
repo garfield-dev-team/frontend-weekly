37abd9f7041c2db03f7ced779657d94af49e3e94
dongjiacheng <dongjiacheng@corp.netease.com>
2023-04-18 19:05:46 +0800

    chore: add commit scripts

--- scripts/commit.ts
+++ scripts/commit.ts
@@ -7,6 +7,7 @@ const execa = promisify(exec);
 
 async function main() {
   consola.info("Get local change...");
+
   // git diff 可选参数：
   // `--name-only`: 只显示文件名，而不显示更改的具体内容
   // `--diff-filter=d`：只显示被修改或已删除的文件，过滤掉新增的文件
@@ -18,13 +19,16 @@ async function main() {
       `${changes.length} md files found, please commit then separately!`
     );
   }
+
   consola.info(`Committing local change: ${changes[0]}...`)
+
   let chunk = changes[0].replace(/.*\/(.*)\/(.*)$/, "$1");
   if (chunk.includes("welcome")) {
     chunk = "pinned";
   }
   // 下面是 `git add .` 和 `git commit` 两个命令的缩写
   await execa(`git commit -am ":sparkles: feat: update ${chunk} section"`);
+
   consola.success(
     'Commit successfully! Run "git push" to push to remote repository'
   );
